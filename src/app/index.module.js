a7977f032dd0dc6ccf32b43e9f97d0930f7687cc
00tanveer <tanveer_fh09@yahoo.com>
2018-07-24 12:40:43 +0600

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularjs', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
